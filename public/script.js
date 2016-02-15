87529f9f829607c61dc478259fa0efad9e5037f3
Rock Zhang <iamgtskk@gmail.com>
2016-02-15 14:15:08 +0800

    添加时候震动屏幕的配置项，默认关闭

--- public/script.js
+++ public/script.js
@@ -177,5 +177,6 @@ $(document).ready(function() {
 
     // POWER MODE
     POWERMODE.colorful = true; // make power mode colorful
+    POWERMODE.shake = false; // activate shake effect
     document.body.addEventListener('input', POWERMODE);
 });
